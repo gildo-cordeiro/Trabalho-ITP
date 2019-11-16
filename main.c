2fb2a0d5caf482827ec1025402d8dd213e397f42
Gildo Cordeiro <gildo.duartec@gmail.com>
2019-11-16 13:29:39 -0300

    Gerando o triangulo e failhando no quadrado

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main(int argc, char const *argv[]){
 	printf("\nDigite o que deseja fazer: \n");
 	printf("P - inserir ponto\n");
 	printf("L - Inserir linhal\n");
+	printf("T - Inserir Triangulo\n");
+	printf("Q - Inserir Quadrado\n");
 	printf("S - sair do programa\n");
 	scanf("%c", &c);
 	if (c == 'P') {
@@ -30,6 +32,35 @@ int main(int argc, char const *argv[]){
 		p2 = definirPonto(x2, y2);
 
 		PPM imagem = desenharLinha(p1, p2, lerArquivo());
+		gerarImagem(imagem);
+	}else if (c == 'T') {
+		Ponto p1, p2, p3;	
+		int x1, x2, x3, y1, y2, y3;
+
+		printf("Informe a linha na forma X1, Y1, X2 e Y2:\n");
+		scanf("%d %d %d %d %d %d",&x1,&y1,&x3, &x2, &y2, &y3);
+
+		p1 = definirPonto(x1, y1);
+		p2 = definirPonto(x2, y2);
+		p3 = definirPonto(x3, y3);
+
+		PPM imagem = desenharTriangulo(p1, p2, p3, lerArquivo());
+		gerarImagem(imagem);
+
+		//essa merda desse quadrado não sai
+	}else if (c == 'Q') {
+		Ponto p1, p2, p3, p4;	
+		int x1, x2, z1,w1, y1, y2, z2, w2;
+
+		printf("Informe a linha na forma X1, Y1, X2 e Y2:\n");
+		scanf("%d %d %d %d %d %d %d %d", &x1, &y1, &x2, &y2, &z1, &z2, &w1, &w2);
+
+		p1 = definirPonto(x1, y1);
+		p2 = definirPonto(x2, y2);
+		p3 = definirPonto(z1, z2);
+		p4 = definirPonto(w1, w2);
+
+		PPM imagem = desenharQuadrado(p1, p2, p3, p4, lerArquivo());
 		gerarImagem(imagem);
 	}
 	return 0;
